ee48b495f2c18cd8aac19eda9f84eff35eee28da
onisoc <12600488+onisoc@users.noreply.github.com>
2022-10-21 19:41:40 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "mysql";
+static const char* process_to_filter = "mysqld";
 
 /*
  * Get a directory name given a DIR* handle
